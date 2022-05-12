88db837ff8cda4414b2b71fd651634aa0e7654ab
Filippo De Berardinis <filippodeb22@libero.it>
2022-05-12 12:55:26 +0200

    refresh local

--- data_downloader/data_downloader_script.py
+++ data_downloader/data_downloader_script.py
@@ -6,4 +6,3 @@ from tickers import EU_tickers_trading212, NASDAQ100_tickers
 md = MarketData()
 md.get_returns(tickers=EU_tickers_trading212, years_of_observations=1).to_excel("EUROSTOXX_ret_250.xlsx")
 md.get_returns(tickers=NASDAQ100_tickers, years_of_observations=1).to_excel("NASDAQ100_ret_250.xlsx")
-
