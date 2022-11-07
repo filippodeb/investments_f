25605b563bd7e5d142105c5e3a79e0ea03fef43b
Filippo De Berardinis <filippodeb22@libero.it>
2022-11-08 00:47:01 +0100

    rebalance nov

--- data_downloader/alpaca_market_data.py
+++ data_downloader/alpaca_market_data.py
@@ -2,11 +2,8 @@
 import asyncio
 import logging
 from typing import Any, Dict, Tuple
-
 import pandas as pd
 
-
-import pandas as pd
 from alpaca.data import (
     Adjustment,
     StockBarsRequest,
